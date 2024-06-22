b3de1ef5ab62b021b3af00e01d03f0dc01caec37
TripleAAA26 <96.azim.96@gmail.com>
2024-06-22 19:05:18 +0500

    Project Organization

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,7 +4,7 @@ export default function Navigation() {
     return (
         <ul>
             <li>
-                <Link href='/'>Home</Link>
+                <Link href='/public'>Home</Link>
             </li>
             <li>
                 <Link href='/cabins'>Cabins</Link>
