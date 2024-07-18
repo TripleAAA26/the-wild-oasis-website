b2b2779d23f470dc4f647bfbb9b06a0c8c1f4e65
TripleAAA26 <96.azim.96@gmail.com>
2024-07-18 18:18:17 +0500

    Using the Context API for State Management

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const josefin = Josefin_Sans({ subsets: [ 'latin' ], display: 'swap' })
 
 import '@/app/_styles/globals.css'
 import Header from '@/app/_components/Header'
+import { ReservationProvider } from '@/app/_components/ReservationContext'
 
 export const metadata = {
     title: {
@@ -24,7 +25,9 @@ export default function RootLayout({ children }) {
                 <Header />
                 <div className='flex-1 px-8 py-12 grid'>
                     <main className='max-w-7xl mx-auto w-full'>
-                        {children}
+                        <ReservationProvider>
+                            {children}
+                        </ReservationProvider>
                     </main>
                 </div>
             </body>
