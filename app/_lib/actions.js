eaad009fef1a87c1ece34893e917f18453c4112c
TripleAAA26 <96.azim.96@gmail.com>
2024-07-22 19:35:57 +0500

    Manual Cache Revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,7 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
+
 import { auth, signIn, signOut } from '@/app/_lib/auth'
 import { supabase } from '@/app/_lib/supabase'
 
@@ -20,6 +22,8 @@ export async function updateGuest(formData) {
         .eq('id', session.user.guestId)
 
     if (error) throw new Error('Guest could not be updated')
+
+    revalidatePath('/account/profile')
 }
 
 export async function signInAction() {
