e0c23c4579c5f8297f119b1d247143cfb70b6704
TripleAAA26 <96.azim.96@gmail.com>
2024-07-29 16:18:05 +0500

    Creating a New Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -7,6 +7,33 @@ import { auth, signIn, signOut } from '@/app/_lib/auth'
 import { supabase } from '@/app/_lib/supabase'
 import { getBookings } from '@/app/_lib/data-service'
 
+export async function createBooking( bookingData, formData ) {
+    const session = await auth()
+    if (!session) throw new Error('You must be logged in')
+
+    const newBooking = {
+        ...bookingData,
+        guestId: session.user.guestId,
+        numGuests: Number(formData.get('numGuests')),
+        observations: formData.get('observations').slice(0, 1000),
+        extrasPrice: 0,
+        totalPrice: bookingData.cabinPrice,
+        isPaid: false,
+        hasBreakfast: false,
+        status: 'unconfirmed',
+    }
+
+    const {  error } = await supabase
+        .from('bookings')
+        .insert([ newBooking ])
+
+    if (error) throw new Error('Booking could not be created')
+
+    revalidatePath(`/cabins/${bookingData.cabinId}`)
+
+    redirect('/cabins/thankyou')
+}
+
 export async function updateGuest(formData) {
     const session = await auth()
     if (!session) throw new Error('You must be logged in')
